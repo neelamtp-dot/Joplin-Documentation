d42fd83c77ddefbf50c246e92e2e46d291434ef4
Naveen <naveenneealamtp@gmail.com>
2026-09-05 22:33:42 +0530

    fix broken and deleted intro page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -109,8 +109,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Getting Started',
+                to: '/docs/tutorial/getting-started',
               },
             ],
           },
